653cf8651e8cf9cda570948193698b093766b549
Nick Düsterhus <duesterhus.nick@t-online.de>
2018-07-15 08:18:00 +0200

    Benchmark update

--- src/main/java/utils/Benchmark.java
+++ src/main/java/utils/Benchmark.java
@@ -1,20 +1,51 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
 
 public class Benchmark {
 	private static final String service = "http://dbpedia.org/sparql";
 	private List<Resource> subject = new ArrayList<Resource>(); 
+	private Model model = ModelFactory.createDefaultModel();
+	private int totalStatements = 0;
+	private int valid = 0;
+	private int sizeModel = 0;
 	
 	
 	
+	public Benchmark(String modelPath, String category) {
+		 initializeSubjects("Presidents_of_the_United_States");
+		 File m = new File(modelPath);
+		 try {
+			model.read(new FileInputStream(m),null, "TTL");
+			StmtIterator it =  model.listStatements();
+			while (it.hasNext()) {
+			     Statement stmt = it.next();
+			     if(subject.contains(stmt.getSubject())) {
+			    	 sizeModel++;
+			    	 
+			     }
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	private void initializeSubjects(String category) {
 		String q = "SELECT ?subject {?subject <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:" + category + "> }";
@@ -24,17 +55,58 @@ public class Benchmark {
 			QuerySolution s = rs.nextSolution();
 			subject.add(s.getResource("subject"));
 		}	
-		System.out.println(subject);
+	}
+	
+	
+	private void benchmark(List<String> dumpPaths) {
+		for(String path : dumpPaths) {
+			benchmarkForDump(path);
+		}
+		
+		System.out.println("The model contains " + sizeModel + "statements of given category. " + valid + " are classified as valid by the dumps. All dumps combined contain " + totalStatements + "statements of given category"  );
+	}
+	
+	private void benchmarkForDump(String file) {
+		 File dump = new File(file);
+		 Model dumpModel = ModelFactory.createDefaultModel() ;
+		 int validDump = 0;
+		 int totalDump = 0;
+		 try {
+			dumpModel.read(new FileInputStream(dump),null, "TTL");
+			StmtIterator it =  dumpModel.listStatements();
+			while (it.hasNext()) {
+			     Statement stmt = it.next();
+			     if(subject.contains(stmt.getSubject())) {
+			    	 totalStatements++;
+			    	 totalDump++;
+			    	 if(model.contains(stmt)) {
+			    		 validDump++;
+			    		 valid++;
+			    	 }
+			    		 
+			     }
+			     
+			}
+			System.out.println(dump.toString() + "contains " + totalDump + "statements of given category and the given model contains " + validDump + " of them." );
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
 	
 	public static void main(String[] args) {
 	
-		Benchmark b = new Benchmark();
-		b.initializeSubjects("Presidents_of_the_United_States");
+		Benchmark b = new Benchmark("src/main/resources/model.ttl", "Presidents_of_the_United_States" );
+		List<String> dumps = Arrays.asList("src/main/resources/disambiguations_en.ttl", "src/main/resources/instance_types_en.ttl", "src/main/resources/instance_types_transitive_en.ttl",
+										    "src/main/resources/labels_en.ttl","src/main/resources/long_abstracts_en.ttl", "src/main/resources/mappingbased_literals_en.ttl", 
+											"src/main/resources/mappingbased_objects_en.ttl", "src/main/resources/persondata_en.ttl", "src/main/resources/specific_mappingbased_properties_en.ttl", "src/main/resources/transitive_redirects_en.ttl");
+		
 	 
-	
+		b.benchmark(dumps);
 	}	
 		
 		
